8b9b7027706a142eba776ab2c2e081ebb049f627
AllenGuitar <sarita.sotaquira@gmail.com>
2023-04-16 21:18:04 -0500

    Calculadora

---
+++ Calculadora/Operations.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Operations
+    {
+        //    console.writeline("1. suma");
+        //    console.writeline("2. resta");
+        //    console.writeline("3. multiplicacion");
+        //    console.writeline("4. division");
+        //    console.writeline("5. cuadrado");
+        //    console.writeline("6. raiz cuadrada");
+
+        public int opcuser { get; set; }
+        public double number1 {  get; set; }
+        public double number2 { get; set; }
+
+        double solution;
+
+        public Operations(int opcuser,double number1, double number2) {
+
+            this.opcuser = opcuser;
+            this.number1 = number1;
+            this.number2 = number2;
+
+            switch (opcuser){
+                case 1:
+                     solution= number1 + number2;                  
+                     break;
+                
+                case 2:
+                     solution = number1 - number2;
+                     break;
+
+                case 3:
+                     solution = number1 * number2;
+                     break;
+
+                case 4:
+                    solution = number1 / number2;
+                    break;
+
+                case 5:
+                    solution = number1 * number1;
+                    break;
+
+                case 6:
+                    solution = Math.Sqrt(number1);
+                    break;
+
+
+            }
+
+            PrintResults print = new PrintResults(solution);
+
+
+
+        }
+
+        
+
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("Por favor ingrese la operacion que desea realizar");
+            Console.WriteLine("1. Suma");
+            Console.WriteLine("2. Resta");
+            Console.WriteLine("3. Multiplicacion");
+            Console.WriteLine("4. Division");
+            Console.WriteLine("5. Cuadrado");
+            Console.WriteLine("6. Raiz cuadrada");
+            Console.WriteLine("-------------------------------------------------");
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            string opcuserstr = Console.ReadLine();
+            int opcuser;
+
+            int.TryParse(opcuserstr, out opcuser);
+
+            while (opcuserstr == "" || int.TryParse(opcuserstr, out opcuser) == false || opcuser <= 0 || opcuser > 6)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("El valor ingresado es erroneo, Por favor digite un valor");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                opcuserstr = Console.ReadLine();
+            }
+
+            
+            int.TryParse(opcuserstr, out opcuser);
+            
+
+            string convnum1;
+            string convnum2;
+
+            double num1;
+            double num2;
+
+            Console.WriteLine("Por favor ingrese el primer numero");
+            convnum1 = Console.ReadLine();
+
+            while (convnum1 == "" || double.TryParse(convnum1, out num1) == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("El valor ingresado es incorrecto, por favor digite un valor");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                convnum1 = Console.ReadLine();
+            }
+            
+            double.TryParse(convnum1, out num1);
+
+            if (opcuser == 5 || opcuser == 6)
+            {
+                num2 = 0;
+            }
+            else
+            {
+                Console.WriteLine("Por favor ingrese el segundo numero");
+                convnum2 = Console.ReadLine();
+
+                while (convnum2 == "" || double.TryParse(convnum2, out num2) == false)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("El valor ingresado es incorrecto, por favor digite un valor");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    convnum2 = Console.ReadLine();
+                }
+                
+                double.TryParse(convnum2, out num2);
+
+
+            }
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Operations operation = new Operations(opcuser, num1, num2);
+
+
+
+
+        }
+
+    }
+}
